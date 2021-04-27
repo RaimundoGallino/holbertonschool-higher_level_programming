3e2bcea9c5a2e9fe80939bfbfe6ace92d20aa4e4
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 17:22:47 -0300

    Uploading files

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if (c > ord('a')) and (c < ord('z')+1):
+    if ord('c') >= ord('a') and ord('c') <= ord('z'):
         return True
     else:
         return False
