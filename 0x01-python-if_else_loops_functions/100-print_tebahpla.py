6837fbcb35cd705ed2c32c46f5c037a8e9e9ec92
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 20:43:01 -0300

    Uploading files

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter_alphabet in range(ord('z'), ord('a'), -1):
-    if letter_alphabet % 2 == 0:
-        letter_alphabet = ord(letter_alphabet) - 32
-    print("{:c}" .format(letter_alphabet))
+    if letter_alphabet % 2 != 0:
+        letter_alphabet = letter_alphabet - 32
+    print("{:c}" .format(letter_alphabet), end="")
