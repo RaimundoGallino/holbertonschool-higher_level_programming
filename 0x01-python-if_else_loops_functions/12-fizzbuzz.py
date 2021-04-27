dba599c61c76ed5897a6a96ef46c5b763c12eb42
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 20:16:18 -0300

    Uploading files

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for fb in range(1, 100):
+    for fb in range(1, 101):
         if fb % 3 == 0 and fb % 5 == 0:
             print("FizzBuzz ", end='')
         elif fb % 3 == 0:
@@ -11,4 +11,3 @@ def fizzbuzz():
             print("{} ".format(fb), end='')
         if fb == 100:
             print(" ")
-    
