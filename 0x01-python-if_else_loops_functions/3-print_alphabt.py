d9a6239c450db8bb9ff3ef1173f4516fa87ed13b
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 15:13:22 -0300

    uploading files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-        continue
-    print("{:c}".format(alpha_letters), end='')
+for letter_alphabet in range(ord('a'), ord('z')+1):
+    letter = chr(letter_alphabet)
+    if letter not in "qe":
+        print(letter, end="")
