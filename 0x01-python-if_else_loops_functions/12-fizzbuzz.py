51a551d4069fc604617b950d085d3e312a8be8f3
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 20:06:54 -0300

    FzzzBZZZZZ hot fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for fb in range(1, 100)
+    for fb in range(1, 100):
         if fb % 3 == 0 and fb % 5 == 0:
             print("FizzBuzz ", end='')
         elif fb % 3 == 0:
