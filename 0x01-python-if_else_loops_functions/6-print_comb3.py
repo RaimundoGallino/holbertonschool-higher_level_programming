6f97c98147d52235bb25ba7b792894e3e43adbe1
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 16:39:49 -0300

    Uploading files

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for alpha in range(9):
+    for alpha2 in range(10):
+        if alpha == alpha2:
+            continue
+        else:
+            if alpha == 8 and alpha2 == 9:
+                print("{}{}, ".format(alpha, alpha2), end='')
+            else:
+                print("{}{}".format(alpha, alpha2))
