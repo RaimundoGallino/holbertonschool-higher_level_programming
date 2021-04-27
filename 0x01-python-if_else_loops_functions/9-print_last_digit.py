4bbff6937bdcf1b2d665728ea25ceb58b4f5199c
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 19:35:29 -0300

    Uploading files

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 9:
-        print("{}".format(number % 10))
-    else:
-        print("{}".format(number))
+    if number < 0:
+        number = -number
+        print("{:d}".format(number % 10))
