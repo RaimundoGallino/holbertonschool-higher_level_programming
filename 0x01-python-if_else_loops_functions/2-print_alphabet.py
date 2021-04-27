92b85383de2209bf9ac0cc97d25cb8de05e9f50d
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 15:09:22 -0300

    Uploading files

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3  
-for alpha_letters in range(97, 123):
-    print("{}".format(chr(alpha_letters)), end='')
+#!/usr/bin/python3
+for alpha_letters in range(ord('a'), ord('z')+1):
+    print("{:c}".format(alpha_letters), end='')
