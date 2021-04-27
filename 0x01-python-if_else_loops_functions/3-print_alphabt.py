9b78a8d9a9e8c7c5083780ba8dd099c17e8970bb
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 15:20:17 -0300

    Uploading changes

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter_alphabet in range(ord('a'), ord('z')+1):
-    if letter_alphabet not in "qe":
+    if chr(letter_alphabet) not in "qe":
         print(chr(letter_alphabet), end='')
