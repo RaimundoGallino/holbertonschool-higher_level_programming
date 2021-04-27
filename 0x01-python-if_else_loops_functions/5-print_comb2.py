44409d493c8689c896c8944b8569b56a18420fdf
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 16:25:04 -0300

    uploading files

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for alpha in range(99):
-    print("{:02d}".format(alpha))
+for alpha in range(0, 99):
+    if alpha < 99:
+        print("{:02d}, ".format(alpha), end='')
+    else:
+        print("{:02d}, ".format(alpha))
