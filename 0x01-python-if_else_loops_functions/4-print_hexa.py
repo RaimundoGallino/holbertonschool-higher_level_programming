0dcf87f76f043dbc8c40b158c97b6340ebb24e03
RaimundoGallino <raigallino0@gmail.com>
2021-04-27 15:55:24 -0300

    Uploading files

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for alpha in range(0, 98):
-    print("{:d} =".format(alpha), end='')
-    print("{:d}".format(hex(alpha))
+for alpha in range(99):
+    print("{} = {}".format(alpha, hex(alpha))
