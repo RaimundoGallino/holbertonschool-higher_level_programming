adf9c2f45095241edb6b35970edac7b068741c60
RaimundoGallino <raigallino0@gmail.com>
2021-04-29 16:42:23 -0300

    adding a conditional

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-if __name__:    
+if __name__ == "__main__":
+    import hidden_4
+
+for count in dir(hidden_4)
+    if count[:2] != "__":
     print(dir(hidden_4))
