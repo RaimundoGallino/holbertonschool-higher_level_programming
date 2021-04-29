a80c445bfcd2a3f79cbc47d8aca39d8609e55057
RaimundoGallino <raigallino0@gmail.com>
2021-04-29 15:56:46 -0300

    Uploading files

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     if (argc == 0):
         print("0")
     for count in range(1, argc):
-        sum += int(argv[count])
+        int(sum) += int(argv[count])
     print("{}".format(sum))
