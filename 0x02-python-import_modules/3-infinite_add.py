123ef7acce96c20d4b0a15db999bd02f0261741a
RaimundoGallino <raigallino0@gmail.com>
2021-04-29 15:47:26 -0300

    uploading files

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc == len(argv)
+    if (argc == 0):
+        print("0")
+    for count in range(1, argc + 1):
+        print("{}".format(argv[count]))
