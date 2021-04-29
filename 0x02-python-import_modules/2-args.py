0fe6bfff5b74091bfbfde9f79ef83dc859e220ef
RaimundoGallino <raigallino0@gmail.com>
2021-04-29 15:05:54 -0300

    Uploading files

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv) -1
+    if argc == 0:
+        print("0 arguments.")
+    elif (argc == 1):
+        print("1 argument:\n 1: {}".format(argv))
+    else:
+        print("{} arguments:".format(argc))
+        for count in range (argc):
+            print("{}: {}".format(i, argv[i]))
