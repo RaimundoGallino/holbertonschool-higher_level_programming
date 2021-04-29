e0b443c8392707f244cee39126e4e0a0ce6e7785
RaimundoGallino <raigallino0@gmail.com>
2021-04-29 13:58:10 -0300

    Uploading files

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+    from calculator_1 import __all__
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} - {} = {}".format(a, b, sub(a,b)))
+    print("{} * {} = {}".format(a, b, mul(a,b)))
+    print("{} / {} = {}".format(a, b, div(a,b)))
