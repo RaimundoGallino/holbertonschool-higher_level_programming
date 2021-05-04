6e4a98b9c845933411e7316e09ce4820e68fa412
RaimundoGallino <raigallino0@gmail.com>
2021-05-04 16:54:55 -0300

    Uploading files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list
-    if (idx < 0 || idx > len(my_list)):
+    if (idx < 0 or idx > len(my_list)):
         return new_list
     for i in range(idx):
         new_list[i] = element
 
     return new_list
-    
-
-
