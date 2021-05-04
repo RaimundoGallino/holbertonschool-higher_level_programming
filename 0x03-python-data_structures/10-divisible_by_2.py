5eb411b629304185db7d22601bca395430c410a0
RaimundoGallino <raigallino0@gmail.com>
2021-05-04 19:47:40 -0300

    Uploading files

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    boolean = []
+
+    for element in my_list:
+        if (element % 2 == 0)
+            boolean.append(True)
+        else:
+            boolean.append(False)
+    return (boolean)
