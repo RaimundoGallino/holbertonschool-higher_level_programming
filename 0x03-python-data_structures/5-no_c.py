50ab51fbc1beb2956fcdb266d8a64bbd5b703c6c
RaimundoGallino <raigallino0@gmail.com>
2021-05-04 12:51:49 -0300

    Uploading files

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string
-    for i in range(len(new_string))
-        if chr(new_string) not in "cC":
-            return new_string
+    new_string = ""
+    for element in my_string:
+        if element not in "cC":
+            new_string += element
+    return new_string
 
