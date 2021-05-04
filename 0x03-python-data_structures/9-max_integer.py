853bee7bdcaca3dcbc9fe73f9e0b3995229d1ef4
RaimundoGallino <raigallino0@gmail.com>
2021-05-04 19:36:34 -0300

    Uploading files

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return None
 
     my_list.sort()
-    return(my_list[-1])
+    return (my_list[-1])
