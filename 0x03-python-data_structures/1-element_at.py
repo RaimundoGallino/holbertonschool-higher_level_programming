f1f300ba3ef9a0e3366608aec931928afd81b2cd
RaimundoGallino <raigallino0@gmail.com>
2021-05-04 15:52:30 -0300

    Uploading files

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
     if (idx < 0 || idx > len(my_list)):
         return None
-    print("Element at index {} is {}".format(my_list[:idx]))
+    print("Element at index {:d} is {:d}".format(idx, my_list[:idx]))
