080d0c64882262aec3ae20ed14213dc630005255
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 17:18:37 -0300

    Uploading files

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    value = 0
+    if roman_string:
+        for letter in roman_string:
+            if letter == "I": value += 1
+            if letter == "V": value += 5
+            if letter == "X": value += 10
+            if letter == "L": value += 50
+            if letter == "V": value += 100
+            if letter == "D": value += 500
+            if letter == "M": value += 1000
+        return value
+    else:
+        return None
+
+        
