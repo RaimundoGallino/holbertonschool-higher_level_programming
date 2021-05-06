ef3da4ef852e7ecf1907839b3031b7cd340ce39c
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 16:19:51 -0300

    Uploading files

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = a_dictionary
-    for key in new_dic:
-        new_dic[key] *= 2
+    new_dic = {}
+    for key in a_dictionary:
+        new_dic[key] = a_dictionary[key] * 2
     return new_dic
