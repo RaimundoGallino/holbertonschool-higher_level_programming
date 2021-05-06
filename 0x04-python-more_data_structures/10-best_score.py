3e874998b1de0eb65462767f015838584d36927d
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 16:24:53 -0300

    Uploading files

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary)
+    if a_dictionary.items():
+        return max(a_dictionary)
+    else:
+        return a_dictionary
