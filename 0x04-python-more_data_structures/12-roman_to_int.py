3bcc0b458fdc0a4ba61cb56a2a860d2b0cc12e70
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 17:33:01 -0300

    Uploading files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,22 @@ def roman_to_int(roman_string):
     value = 0
     if roman_string:
         for letter in roman_string:
-            if letter == "I": value += 1
-            if letter == "V": value += 5
-            if letter == "X": value += 10
-            if letter == "L": value += 50
-            if letter == "C": value += 100
-            if letter == "D": value += 500
-            if letter == "M": value += 1000
+            if letter > letter -1:
+                if letter == "I": value += 1
+                if letter == "V": value += 5
+                if letter == "X": value += 10
+                if letter == "L": value += 50
+                if letter == "C": value += 100
+                if letter == "D": value += 500
+                if letter == "M": value += 1000
+            else:
+                if letter == "I": value -= 1
+                if letter == "V": value -= 5
+                if letter == "X": value -= 10
+                if letter == "L": value -= 50
+                if letter == "C": value -= 100
+                if letter == "D": value -= 500
+                if letter == "M": value -= 1000
         return value
     else:
         return None
