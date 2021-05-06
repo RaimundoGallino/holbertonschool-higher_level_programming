11a02881a9c7d643c67811c20c7045107dc2c53f
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 13:04:30 -0300

    Uploading files

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+  #!/usr/bin/python3
+  def uniq_add(my_list=[]):
+    sum_of_unique_numbers = []
+    unique_numbers = set(my_list)
+    for number in unique_numbers:
+        sum_of_unique_numbers += number
+    return sum_of_unique_numbers
