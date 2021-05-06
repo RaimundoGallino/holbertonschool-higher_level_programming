b418bf1df092f998829160aebaf4e077e66dac06
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 13:42:52 -0300

    Uploading files

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for element in set_1:
-        if element in set_2:
-            return element
+    res = set(set_1 & set_2)
+    return res
