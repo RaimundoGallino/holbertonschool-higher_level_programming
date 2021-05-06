5dc3b93f1c88609326459e79595a5c079c0fcecd
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 12:56:00 -0300

    Uploading files

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    replaced_list = []
+    for i in range(0, len(my_list))
+        if my_list[i] == search:
+            replaced_list.append(replace)
+        else:
+            replaced_list.append(my_list[i])
+    return replaced_list
