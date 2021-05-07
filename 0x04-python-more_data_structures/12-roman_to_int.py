72dbf8038fca9d4620ae85e029ffc335b442a20d
RaimundoGallino <raigallino0@gmail.com>
2021-05-06 21:46:46 -0300

    Uploading files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     value = 0
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string is not str: return 0
+    if type(roman_string) is not str: 
+        return 0
     if roman_string:
         for idx in range(len(roman_string)):
             if (idx + 1 < len(roman_string)):
