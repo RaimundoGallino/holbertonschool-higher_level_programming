4ca011aacaaec29917564c4a1671c5fc4e6d4de5
RaimundoGallino <raigallino0@gmail.com>
2021-05-17 18:30:38 -0300

    Fixing o and 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i=0
+    i = 0
     try:
         for i in range(x):
             print(my_list[i], end='')
-            i+=1
+            i += 1
     except IndexError:
         print()
         return i
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-            l+=1
+            l += 1
         except (ValueError, TypeError):
             continue
     print()
