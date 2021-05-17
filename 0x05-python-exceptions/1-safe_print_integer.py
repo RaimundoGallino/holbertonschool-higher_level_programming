f389aa41fb5ed552c933c70c1cc4a75ac497c31d
RaimundoGallino <raigallino0@gmail.com>
2021-05-17 13:45:07 -0300

    First Files

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, IndexError):
+    except (ValueError, TypeError):
         return False
 
     return True
