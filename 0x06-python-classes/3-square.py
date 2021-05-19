c10abee5a1fd849c83362eab0490315a7dd45bea
RaimundoGallino <raigallino0@gmail.com>
2021-05-18 22:02:18 -0300

    Adding files

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
         '''__init__ function'''
         self.__size = 0
         self.__size = size
-        if (size <= 0):
+        if (size < 0):
             raise ValueError("size must be >= 0")
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
