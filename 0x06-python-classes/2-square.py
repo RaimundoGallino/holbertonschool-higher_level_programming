e7125dc69f02f747472ed93fc5693ba153f46f24
RaimundoGallino <raigallino0@gmail.com>
2021-05-18 21:57:21 -0300

    Adding files

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,6 @@ class Square:
         '''__init__ function'''
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        if (size < 0):
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
