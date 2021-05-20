dabc73c73274ad26c518953bcfbfd95dd0db3373
RaimundoGallino <raigallino0@gmail.com>
2021-05-20 17:20:24 -0300

    Adding files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if isinstance(a, float): a = int(a)
+    if isinstance(b, float): b = int(b)
+    if isinstance(a, int) is False:
+        raise TypeError("a must be an integer")
+    elif isinstance(b, int) is False:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
