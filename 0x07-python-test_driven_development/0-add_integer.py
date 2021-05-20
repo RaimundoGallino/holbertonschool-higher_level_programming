2815d5d4978287060c8a7ff08fbebd3417f91e15
RaimundoGallino <raigallino0@gmail.com>
2021-05-20 17:22:00 -0300

    Adding files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    if isinstance(a, float): a = int(a)
-    if isinstance(b, float): b = int(b)
-    if isinstance(a, int) is False:
-        raise TypeError("a must be an integer")
-    elif isinstance(b, int) is False:
-        raise TypeError("b must be an integer")
-    else:
-        return a + b
+if __name__ == "__main__":
+    def add_integer(a, b=98):
+        if isinstance(a, float): a = int(a)
+        if isinstance(b, float): b = int(b)
+        if isinstance(a, int) is False:
+            raise TypeError("a must be an integer")
+        elif isinstance(b, int) is False:
+            raise TypeError("b must be an integer")
+        else:
+            return a + b
