311c939b4c1409c766122248039ea6e042c286cd
RaimundoGallino <raigallino0@gmail.com>
2021-05-24 20:44:26 -0300

    Adding files

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -65,4 +65,4 @@ class Rectangle:
         return therectboy
 
     def __repr__(self):
-        return "Rectangle({},{})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
