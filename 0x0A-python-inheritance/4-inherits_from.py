7edcf5e97e087e33c3b883d3ea16e3e3b3d021a7
RaimundoGallino <raigallino0@gmail.com>
2021-06-01 19:10:26 -0300

    correcting 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,6 +7,8 @@ from the specified class; otherwise False.
 
 
 def inherits_from(obj, a_class):
+    """inherits_from function"""
+
     if (isinstance(obj, a_class)):
         return True
     else:
