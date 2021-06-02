ff5670b62b7aeaffd374e0da460887dff46b2879
RaimundoGallino <raigallino0@gmail.com>
2021-06-01 21:30:55 -0300

    correcting 4

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+MyInt that inherits from int
+"""
+
+class MyInt(int):
+    """MyInt is a rebel. MyInt has == and != operators inverted"""
+
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
