2056ab64a705647a19542af7768c3326616ee5d0
RaimundoGallino <raigallino0@gmail.com>
2021-06-01 23:00:09 -0300

    correcting 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,8 +3,10 @@
 MyInt that inherits from int
 """
 
+
 def add_attribute(objclass, attr, newattr):
     """MyInt that inherits from int"""
+
     if not hasattr(objclass, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(objclass, attr, newattr)
