db4e6e9e8684b7d59e0f643133b0e32f7b4108dd
RaimundoGallino <raigallino0@gmail.com>
2021-06-02 18:10:16 -0300

    Adding files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ function that reads a text file (UTF8) and prints it to stdout
 
 
 def write_file(filename="", text=""):
+    """definition of write_file"""
     with open(filename, "w", encoding="utf-8") as f:
         fulltext = f.write(text)
     return(fulltext)
