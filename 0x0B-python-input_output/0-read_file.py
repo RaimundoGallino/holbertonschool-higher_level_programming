4f9d925aabf4e5df6d4b69b0763b7d40dec81135
RaimundoGallino <raigallino0@gmail.com>
2021-06-02 17:30:04 -0300

    Adding files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ function that reads a text file (UTF8) and prints it to stdout
 
 def read_file(filename=""):
     """defining readfile function"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, encoding="UTF8") as f:
         lines = f.read()
