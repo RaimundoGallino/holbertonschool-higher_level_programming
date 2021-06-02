3e232c72e52720f7a30d3a77cd84f67a7e8a75dd
RaimundoGallino <raigallino0@gmail.com>
2021-06-02 18:15:09 -0300

    Adding files

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,7 @@ function that reads a text file (UTF8) and prints it to stdout
 
 
 def append_write(filename="", text=""):
+    """defining readfile function"""
     with open(filename, "a", encoding="utf-8") as f:
         fulltext = f.write(text)
     return(fulltext)
