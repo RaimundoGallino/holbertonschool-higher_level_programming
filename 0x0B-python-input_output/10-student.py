9bc42d0553ab390ce7401271420567b8812fb6e4
RaimundoGallino <raigallino0@gmail.com>
2021-06-02 19:07:10 -0300

    Adding files

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+class Student:
+    """class definition"""
+
+    def __init__(self, first_name, last_name, age):
+        """inicialization of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """inicialization of the class"""
+        return (self.__dict__)
+    
+
