cac9a87085d13b29adaecdc16cd1fe8637d1ad05
RaimundoGallino <raigallino0@gmail.com>
2021-07-13 19:32:18 -0300

    uploading task 14

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,6 +5,6 @@
 -- script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows 
 FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
