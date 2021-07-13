b9659fc0f8755c5b752d398972cd830c3da70fa0
RaimundoGallino <raigallino0@gmail.com>
2021-07-13 20:26:45 -0300

    uploading task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
+FROM tv_shows
+
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
