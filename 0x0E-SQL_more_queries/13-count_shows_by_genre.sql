9eecfe75f5a5e3398547ec94c4dd67213f47ab93
RaimundoGallino <raigallino0@gmail.com>
2021-07-13 19:28:23 -0300

    uploading task 14

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows 
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
