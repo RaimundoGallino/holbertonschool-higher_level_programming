53a45aedb56772249ae833664dbc4375295e03ce
RaimundoGallino <raigallino0@gmail.com>
2021-08-02 18:20:59 -0300

    correcting semistandard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const myVar = 'C is Fun';
-const myVar1 = 'Python is cool';
-const myVar2 = 'JavaScript is amazing';
+let myVar = 'C is Fun';
+let myVar1 = 'Python is cool';
+let myVar2 = 'JavaScript is amazing';
 console.log(myVar);
 console.log(myVar1);
 console.log(myVar2);
