5128c4c2c6b5d1c5330e40fef2a069f33d8a0532
RaimundoGallino <raigallino0@gmail.com>
2021-08-02 18:48:29 -0300

    correcting task 7

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv[2] == null) {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(process.argv[2]));
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  const n_ocurr = process.argv[2];
-  for (let i = 0; i < n_ocurr; i++) {
+  const nOcurr = process.argv[2];
+  for (let i = 0; i < nOcurr; i++) {
     console.log('C is fun');
   }
 }
