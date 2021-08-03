d3be240d1d3108543779a764bc16c3a1eab281a3
RaimundoGallino <raigallino0@gmail.com>
2021-08-03 16:36:03 -0300

    Correcting task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,14 +3,14 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-      this.height = h;
       this.width = w;
+      this.height = h;
     }
   }
 
   print () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.height));
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
