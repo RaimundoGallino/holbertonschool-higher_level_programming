5801190b0527dd65872b45434022d5f401c56f30
RaimundoGallino <raigallino0@gmail.com>
2021-08-05 14:47:36 -0300

    Uploading files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,10 @@
+import MySQLdb
+
+serv = MySQLdb.connect(host = "localhost", user = "root", passwd = "root")
+
+c = serv.cursor()
+c.execute("SHOW DATABASES")
+l = c.fetchall()
+print l
+l = [ i[0] for i in l ]
+print l
