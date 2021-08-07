9c3487d8fab0954668a3ddc805db93ae60dd7128
RaimundoGallino <raigallino0@gmail.com>
2021-08-07 16:54:33 -0300

    uploading First files

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Show the table states from indicated database"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    serv = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    c = serv.cursor()
+    c.execute("SELECT * FROM states")
+    l = c.fetchall()
+    for i in l:
+        le = len(i[1]) - 1
+        state = i[1]
+        if state == argv[4]:
+            print (i)
