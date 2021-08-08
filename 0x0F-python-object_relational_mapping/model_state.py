794ba75dc12e3c9b767f7ce94d10e93ec47044c3
RaimundoGallino <raigallino0@gmail.com>
2021-08-08 15:57:01 -0300

    uploading 6 task files

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class SomeClass(Base):
+class State(Base):
+    """Creating state class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name =  Column(String(128), nullable=False)
