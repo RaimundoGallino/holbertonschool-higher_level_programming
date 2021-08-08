a7bcba6e383a8b6782fc75051cfa641683da8c30
RaimundoGallino <raigallino0@gmail.com>
2021-08-08 16:06:08 -0300

    correcting pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Show the table states from indicated database"""
 
 from sqlalchemy import Column, Integer, String
@@ -7,8 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Creating state class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
