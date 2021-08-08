f3b66490a9b94ff5a7fce5d737e2db65f19db17d
RaimundoGallino <raigallino0@gmail.com>
2021-08-08 17:02:26 -0300

    adding 7 task

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Show states content
+"""
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format("root", "root", "my_db"),
+                       pool_pre_ping=True)
+
+Base.metadata.create_all(engine)
+
+session = sessionmaker(engine)
+session = Session()
+
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+
+session.close()
