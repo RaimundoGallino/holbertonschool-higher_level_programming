99b526472045ff581d88d0e3709b476129d0a6a8
RaimundoGallino <raigallino0@gmail.com>
2021-08-08 18:21:06 -0300

    adding 10  task

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Show states content
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    search = argv[4]
+
+    element = session.query(State).order_by(State.id).filter_by(name=argv[4]).first()
+    print(element.id)
+
+    session.close()
