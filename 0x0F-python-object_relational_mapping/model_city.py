8b1cb5808373894726e025d28d315ef7868ef12e
RaimundoGallino <raigallino0@gmail.com>
2021-08-08 21:55:41 -0300

    adding 14  task

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,9 +3,7 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
+from model_state import Base
 
 class City(Base):
     """Creating cities class"""
