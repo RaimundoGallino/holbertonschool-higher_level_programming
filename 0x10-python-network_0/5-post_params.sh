91942d8de38ce5b070a427e06b9f455486ff6008
RaimundoGallino <raigallino0@gmail.com>
2021-08-19 21:26:46 -0300

    Correecting all files

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL, 
-#sends a POST request to the passed URL,
-#and displays the body of the response
+#Bash script that takes in a URL, sends a POST request to the passed URL
 curl "$1" -s -H -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
