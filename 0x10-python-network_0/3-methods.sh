e91fd73251c4c03d829d08cad8a87b65a3b31406
RaimundoGallino <raigallino0@gmail.com>
2021-08-19 21:31:07 -0300

    Correecting all files

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl "$1" -s | grep "Allow" | cut -d " " -f2-
+curl "$1" -s | grep "Allow" | cut -d " " -f2
