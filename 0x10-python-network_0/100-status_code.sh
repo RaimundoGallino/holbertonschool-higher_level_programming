74ab6c4a40277b6b3182cbda0bda9d9bd07a0a87
RaimundoGallino <raigallino0@gmail.com>
2021-08-19 21:56:05 -0300

    Adding advances task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -I -o /dev/null "$1" -w "%{http_code}\n"
+curl -s -I -o /dev/null "$1" -w "%{http_code}"
