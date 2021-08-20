28d54de3877867f2a89a712fd36823bf822b0f60
RaimundoGallino <raigallino0@gmail.com>
2021-08-20 17:50:10 -0300

    Adding  7 task

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,7 @@ to the URL and displays the body of the response (decoded in utf-8).
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
     bad_r = requests.get(argv[1])
     if str(bad_r.status_code) != '200':
