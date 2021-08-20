5ce78cb035c612e853c59067c18da247c3584a29
RaimundoGallino <raigallino0@gmail.com>
2021-08-20 15:04:05 -0300

    Adding first tasks

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        resp = response.read()
+        print ('Body response:')
+        print ('\t- type: {}'.format(type(resp)))
+        print ('\t- content: {}'.format(resp))
+        print ('\t- utf-8 content: {}'.format(resp.decode('utf-8')))
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, 
+sends a POST request to the passed URL with the
+email as a parameter, and displays the body
+of the response (decoded in utf-8)
+"""
+
+
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    data = urllib.parse.urlencode({ 'email': argv[2] }).encode()
+
+    req = urllib.request.Request(argv[1], data=data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
