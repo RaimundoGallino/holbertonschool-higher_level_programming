80342ef94c93e8e261d62bb761faf2f7cf278735
RaimundoGallino <raigallino0@gmail.com>
2021-08-20 17:26:29 -0300

    Adding  6 task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request
+to the URL and displays the body of the response (decoded in utf-8).
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.post(argv[1], data={'email':argv[2]})
