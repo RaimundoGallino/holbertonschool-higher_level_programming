56fe2395c900e1396f508aa9e92a53692c6966d9
RaimundoGallino <raigallino0@gmail.com>
2021-09-14 20:58:38 -0300

    Adding task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const id = process.argv[2];
 const request = require('request');
 
-request.get({ url: `https://swapi-api.hbtn.io/api/films/${id}`}, function (err, res, body) {
+request.get({ url: `https://swapi-api.hbtn.io/api/films/${id}` }, function (err, res, body) {
   if (err) {
     console.log(err);
   }
