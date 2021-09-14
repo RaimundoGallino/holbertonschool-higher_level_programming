92890255e3e5037e524b58acecf5c5e07ed7b624
RaimundoGallino <raigallino0@gmail.com>
2021-09-14 19:58:32 -0300

    Adding task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,9 @@ const path = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(path, 'utf8', function (err, data) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(data);
-
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
