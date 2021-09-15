129dd13db8f2b201f04b423fa01654db16fed26a
RaimundoGallino <raigallino0@gmail.com>
2021-09-14 21:12:57 -0300

    Adding task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request.get({ url: url }, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  let count = 0;
+  const json = JSON.parse(body);
+  json.results.forEach(element => {
+    if (element.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      count++;
+    }
+  });
+  console.log(count);
+});
