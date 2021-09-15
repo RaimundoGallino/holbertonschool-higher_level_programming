5e6f33192db58aacf0351a21443406748c117399
RaimundoGallino <raigallino0@gmail.com>
2021-09-14 21:51:39 -0300

    Adding task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
 const url = process.argv[2];
+const path = process.argv[3];
 const request = require('request');
 
 request.get({ url: url }, function (err, res, body) {
   if (err) {
     console.log(err);
   }
-  let count = 0;
-  const json = JSON.parse(body);
-  console.log(json);
+  const fs = require('fs');
+
+  fs.writeFile(path, body, 'utf8', (err) => {
+    if (err) throw err;
+  });
 });
